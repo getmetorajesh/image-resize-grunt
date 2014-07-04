8167d45ed3c1f85eca14ed55672204be2d77b32b
raj <rajesh.krishnan@wyndham.vic.gov.au>
2014-07-04 15:47:38 +1000

    updated with watch and variable for dir paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,10 @@
 module.exports = function(grunt) {
      // Project configuration.
+     var srcPath = 'MyWyndham/test/';
+     var destPath = 'tmp/';
+     var dest720 = destPath+'/720p';
+     var dest100 = destPath+'/100';
+     var dest82 = destPath+'/82';
     var mozjpeg = require('imagemin-mozjpeg');
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -11,9 +16,35 @@ module.exports = function(grunt) {
                 },
             files: [{
                 expand: true,
-                cwd: 'MyWyndham/test/',
-                src: '{,*/*/*}*.{gif,jpeg,jpg,png,JPG}',
-                dest: 'tmp'
+                cwd: srcPath,
+                src: ['**/*.{gif,jpeg,jpg,png,JPG}'],
+                dest: destPath+"/720p"
+            }]
+          },
+          crop100: {
+              options: {
+                width: 100,
+                quality: 1,
+                crop: true
+                },
+            files: [{
+                expand: true,
+                cwd: dest720,
+                src: ['**/*.{gif,jpeg,jpg,png,JPG}'],
+                dest: destPath+'/100'
+            }]
+          },
+    crop82: {
+              options: {
+                width: 82,
+                quality: 1,
+                crop: true
+                },
+            files: [{
+                expand: true,
+                cwd: dest720,
+                src: ['**/*.{gif,jpeg,jpg,png,JPG}'],
+                dest: destPath+'/82'
             }]
           }
     },
@@ -26,26 +57,21 @@ module.exports = function(grunt) {
             },
           files: [{
             expand: true,                  // Enable dynamic expansion
-            cwd: 'tmp',                   // Src matches are relative to this path
-            src: '{,**/}*.{gif,jpeg,jpg,png,JPG}',   // Actual patterns to match
-            dest: 'tmp'                  // Destination path prefix
+            cwd: dest720,                   // Src matches are relative to this path
+            src: ['**/*.{gif,jpeg,jpg,png,JPG}'],   // Actual patterns to match
+            dest: dest720                  // Destination path prefix
         }]
         }
     },
     imageoptim:{
         myJpgs: {
             options: {
-              jpegMini: true,
+              jpegMini: false,
               imageAlpha: true,
               quitAfter: false
             },
-            src: ['tmp']
+            src: [dest720]
         }
-    },
-    img: {
-        task1: {
-            src: 'tmp',
-        },
     },
      image: {
         dynamic: {
@@ -59,34 +85,20 @@ module.exports = function(grunt) {
         },
         files: [{
           expand: true,
-          cwd: 'tmp',
+          cwd: dest720,
           src: ['**/*.{png,jpg,gif,svg,JPG}'],
-          dest: 'tmp'
+          dest: dest720
         }]
       }
      },
-    imgmin: {
-        files: [{
-         expand: true,
-          cwd: 'tmp',
-          src: ['**/*.{png,jpg,gif,svg,JPG}'],
-          dest: 'tmp'
-        }]
-  },
-    image_resize: {
-          crop: {
-              options: {
-                width: 100,
-                quality: 1,
-                crop: true
-                },
-            files: [{
-                expand: true,
-                cwd: 'tmp/',
-                src: '{,*/*/*}*.{gif,jpeg,jpg,png,JPG}',
-                dest: 'tmp/thumbs'
-            }]
-          }
+    watch: {
+      images: {
+        files: [srcPath+'/**/*.{png,jpg,JPG}'],
+        tasks: ['newer:image_resize:resize','newer:imagemin:dynamic','newer:imageoptim:myJpgs','newer:image'],
+        options: {
+          spawn: false,
+        },
+      },
     },
   });
 
@@ -101,8 +113,16 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-smushit');
   grunt.loadNpmTasks('grunt-imgmin');
    grunt.loadNpmTasks('grunt-resize-crop');
-  // Default task(s).
-  grunt.registerTask('default', ['image_resize:resize','imagemin',
-    'imageoptim','img','image','imgmin','resize_crop']);
+ grunt.loadNpmTasks('grunt-contrib-watch');
+ grunt.loadNpmTasks('grunt-newer');
 
+  // Default task(s).
+  grunt.registerTask('minify', ['image_resize:resize','imagemin',
+    'imageoptim','img','image','image_resize:crop100','image_resize:crop82']);
+
+  grunt.registerTask('minify-newer', ['newer:image_resize:resize','newer:imagemin:dynamic',
+    'newer:imageoptim:myJpgs','newer:image']);
+
+  grunt.registerTask('gen_thumb100', ['newer:image_resize:crop100']);
+  grunt.registerTask('gen_thumb82', ['newer:image_resize:crop82']);
 };
